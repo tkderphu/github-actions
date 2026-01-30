b1d38054dad464bdf10252954a6ab1bcc1e9770b
tkderphu <quangphu.re050204@gmail.com>
2026-01-30 21:42:54 +0700

    add server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world this is node js server")
+// server.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Set response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send response body
+  res.end('Hello from plain Node.js!');
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
